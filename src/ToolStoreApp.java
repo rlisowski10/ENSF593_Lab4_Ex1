c449807d2c8dc556c1998d4e7cd287ab41e71745
rlisowski10 <ryan.lisowski@outlook.com>
2019-10-10 15:30:08 -0600

    Cleanup

--- src/ToolStoreApp.java
+++ src/ToolStoreApp.java
@@ -14,13 +14,6 @@ public class ToolStoreApp {
     Scanner scan = new Scanner(System.in);
     boolean showMenu = true;
 
-    // UPDATED FOR RYANS MAIN BRANCH
-    // TEST 2
-    // Test 3
-    // Test 4
-    // Test 5
-    // Test 6
-    // Test 7
     FileManager fileManager = new FileManager();
     Shop shop = fileManager.loadShop();
 
